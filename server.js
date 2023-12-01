0de3e9cfcbd891456bb046a36267cc1d5e6b77ba
rlfxogh0001 <rlfxogh0927@jointree.co.kr>
2023-12-01 16:36:57 +0900

    resolver 구현

--- server.js
+++ server.js
@@ -1,22 +1,69 @@
 import { ApolloServer, gql } from "apollo-server";
 
+let tweets = [
+    {
+        id: "1",
+        text: "firstOne!",
+    },
+    {
+        id: "2",
+        text: "secondOne!",
+    }
+]
+
 const typeDefs = gql`
     type User {
-        id: ID
-        username: String
+        id: ID!
+        username: String!
+        firstName: String!
+        lastName: String
     }
     type Tweet {
-        id: ID
-        text: String
+        id: ID!
+        text: String!
         author: User
     }
     type Query {
-        allTweets: [Tweet]
-        tweet(id: ID): Tweet
+        allTweets: [Tweet!]!
+        tweet(id: ID!): Tweet
+    }
+    type Mutation { #user가 보낸 data를 Mutation 하면 Mutation Type임 => DB, ... [POST, PATCH, DELETE]
+        postTweet(text: String!, userId: ID!): Tweet!
+        deleteTweet(id: ID!): Boolean!
     }
 `;
 
-const server = new ApolloServer({typeDefs})
+const resolvers = {
+    Query: {
+        allTweets() {
+            return tweets;
+        },
+        tweet(_, {id}) {
+            return tweets.find(tweet => tweet.id === id)
+        }
+    },
+    Mutation: {
+        postTweet(_, {text, userId}) { 
+            const newTweet = {
+                id: tweets.length + 1,
+                text, 
+            };
+            tweets.push(newTweet);
+            return newTweet;
+        },
+        deleteTweet(_, { id }) {
+            const tweetIndex = tweets.findIndex(tweet => tweet.id === id);
+            if (tweetIndex === -1) return false;
+        
+            tweets.splice(tweetIndex, 1);
+            return true;
+        }
+        
+    }
+
+}
+
+const server = new ApolloServer({typeDefs, resolvers})
 
 server.listen().then(({url})=>{
     console.log(`Running on ${url}`)
